d4cc989203e8ee22cd106896d4387847668bda71
Darryl H33 <darrylhiiz8@gmail.com>
2023-10-08 14:00:06 +0800

    Update api.php

--- student-api/routes/api.php
+++ student-api/routes/api.php
@@ -25,18 +25,18 @@ Route::post('register', [UserController::class, 'register']);
 
 Route::get('students/search', [StudentController::class, 'search']);
 
-Route::post('importfile', [StudentController::class, 'import']);
-Route::get('exportfile', [StudentController::class, 'export']);
 // Route::middleware(['auth:api'])->get('/students', [StudentController::class, 'index']);
-// Route::group(['middleware' => 'auth:api'], function(){
-// // Route::get('students', [StudentController::class, 'index']);
-// Route::get('students', [StudentController::class, 'index']);
-// });
 
-
+Route::group(['middleware' => 'auth:api'], function(){
 Route::get('students', [StudentController::class, 'index']);
+Route::get('students/search', [StudentController::class, 'search']);
+});
+
 Route::post('students', [StudentController::class, 'store']);
 Route::get('students/{id}', [StudentController::class, 'show']);
 Route::get('students/{id}/edit', [StudentController::class, 'edit']);
 Route::put('students/{id}/edit', [StudentController::class, 'update']);
 Route::delete('students/{id}/delete', [StudentController::class, 'destroy']);
+
+Route::post('importfile', [StudentController::class, 'import']);
+Route::get('exportfile', [StudentController::class, 'export']);
